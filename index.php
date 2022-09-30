52a5657210911870c88b83708077d0850ccae591
dishhaa <dishawasnik02@gmail.com>
2022-10-01 03:10:55 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+include "index.html";
+
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // $name = $_POST["name"];
+    // $email = $_POST["email"];
+    // $website = $_POST["website"];
+    // $comment = $_POST["comment"];
+    // $gender = $_POST["gender"];
+    // echo ($_POST);
+    var_dump($_POST);
+}
